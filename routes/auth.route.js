86b99cd215b17c36fed6bd81e81db0f663b0ff59
rodrigo1619 <00067920@uca.edu.sv>
2022-10-20 18:57:28 -0600

    feat: auth route created

--- routes/auth.route.js
+++ routes/auth.route.js
@@ -1,7 +1,12 @@
 import {Router} from 'express';
 import { check } from 'express-validator';
 import { login } from '../controllers/auth.controller.js';
+import { validarCampos } from '../middlewares/validar-campos.js';
 
 export const routerAuth = Router();
 
-routerAuth.post('/login', login );
+routerAuth.post('/login',[
+    check('correo','correo obligatorio').isEmail(),
+    check('contraseña','contraseña obligatoria').not().isEmpty(), //que la contraseña no este vacia
+    validarCampos
+], login );
--- routes/usuario.route.js
+++ routes/usuario.route.js
@@ -13,7 +13,7 @@ export const routerUsuario = Router();
         //si se mandan solo 2 argumentos se entiende que es ruta y controlador pero si se mandan 3 se entiende que el de en medio es el middleware y se manda como un arreglo
         //el check es para validar que sea y tenga la forma de un correo, para no estar haciendo el monton de expresiones regulares y tener mas limpio el codigo
         routerUsuario.post('/',[
-                check('nombre', 'El nombre es obligatorio').not().isEmpty(),
+                check('nombre', 'El nombre es obligatorio').not().isEmpty(), //que el nombre no este vacio
                 check('contraseña', 'La contraseña debe ser mayor a 6 caracteres').isLength({ min: 6}),
                 //check('correo', 'Correo invalido').isEmail(), //forma anterior       
                 check('correo').custom(emailExiste),
