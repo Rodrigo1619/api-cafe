97303aceb198bf168f7d91aeb880888fe2c4da82
rodrigo1619 <00067920@uca.edu.sv>
2022-10-20 19:22:27 -0600

    feat: login logic almost finished, generate jwt needed

--- controllers/auth.controller.js
+++ controllers/auth.controller.js
@@ -1,9 +1,50 @@
 import { response } from "express"
+import Usuario from '../models/usuario.model.js'
+import bcryptjs from 'bcryptjs'
 
-const login = (req, res=response)=>{
-    res.json({
-        msg:'login oc'
-    })
+const login = async(req, res=response)=>{
+
+    const {correo, contraseña} = req.body;
+
+    try {
+        //verificando que el correo existe
+        const usuario = await Usuario.findOne({correo});
+        if(!usuario){
+            return res.status(400).json({
+                msg:'correo/contraseña no es correcto - correo '
+            })
+        }
+
+        //si el usuario esta activo en la base de datos
+        if(!usuario.estado){ //de otra manera: usuario.estado === false
+            return res.status(400).json({
+                msg:'Usuario inexistente- estado:false'
+            })
+        }
+
+        //verificando contraseña
+        //compara que la contraseña que estamos recibiendo del parametro del req.body sea la misma que tiene el usuario
+        const contraseñaValida = bcryptjs.compareSync(contraseña, usuario.contraseña)
+        if(!contraseñaValida){
+            return res.status(400).json({
+                msg:'correo/contraseña no es correcto - contraseña'
+            })
+        }
+
+
+        //generando el JWT
+
+
+        res.json({
+            msg:'login oc'
+        })
+
+    } catch (error) {
+        console.log(error);
+        return res.status(500).json({
+            msg:'Hable con el administrador'
+        })
+    }
 }
 
 export{
