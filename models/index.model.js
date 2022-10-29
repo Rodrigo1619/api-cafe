335505601f79f86a3dc169174575e9221016caf8
rodrigo1619 <00067920@uca.edu.sv>
2022-10-29 17:17:36 -0600

    feat: idex.js for model created

---
+++ models/index.model.js
@@ -0,0 +1,11 @@
+import Categoria from './categoria.model.js'
+import Role from './role.model.js'
+import Server from './server.js'
+import Usuario from './usuario.model.js'
+
+export{
+    Categoria,
+    Role,
+    Server,
+    Usuario
+}
