3ff9a37b07fc4136afb86275b95f69edc471d08b
rodrigo1619 <00067920@uca.edu.sv>
2022-11-07 20:41:08 -0600

    todo: bug for searching is required

--- controllers/buscar.controller.js
+++ controllers/buscar.controller.js
@@ -1,8 +1,40 @@
 import { request, response } from "express";
 
+const coleccionesPermitidas = [
+    'categoria',
+    'producto',
+    'usuario',
+    'roles'
+]
 const buscar = (req=request, res=response)=>{
-
     const {coleccion, termino} = req.params
+
+    if(!coleccionesPermitidas.includes(coleccion)){
+        return res.status(400).json({
+            msg: `Las colecciones permitidas son: ${coleccionesPermitidas}`
+        })
+    }
+
+    switch(key){
+        case 'categoria':
+
+        break;
+
+        case 'producto':
+
+        break;
+
+        case 'usuario':
+
+        break;
+
+        default: 
+            res.status(500).json({
+                msg: 'Al backend se le ha olvidado implementar esta busqueda'
+        })
+    }
+
+
     res.json({
         coleccion, termino
     })
