505959f97f4c5987442df8f39e553f1da1650ab4
rodrigo1619 <00067920@uca.edu.sv>
2022-12-22 19:35:01 -0600

    feat: validating extension for files

--- controllers/uploads.controller.js
+++ controllers/uploads.controller.js
@@ -14,8 +14,21 @@ const cargarArchivo = (req=request, res=response)=>{
 
 
     const {archivo} = req.files;
+    const nombreCortado = archivo.name.split('.')
+    const extension = nombreCortado[nombreCortado.length - 1] //tomando la ultima posicion seria la extension
 
-    const uploadPath = path.join(__dirname, '../uploads/', archivo.name); //crear directorio de uploads en la raiz de servidor
+    //validando extensiones que vamos a permitir
+    const extensionesPermitidas = ['png', 'jpg', 'jpeg', 'gif']
+    if(!extensionesPermitidas.includes(extension)){
+        return res.status(400).json({
+            msg: `La extension ${extension} no es valida, solamente se aceptan: ${extensionesPermitidas}`
+        })
+    }
+res.json({
+    extension
+})
+
+/*     const uploadPath = path.join(__dirname, '../uploads/', archivo.name); //crear directorio de uploads en la raiz de servidor
                         //archivo.name: name es lo que leemos de como se llama nuestro archivo a subir (se vio en la consola)
     archivo.mv(uploadPath, (err)=> {
         if (err) {
@@ -23,7 +36,7 @@ const cargarArchivo = (req=request, res=response)=>{
     }
     
     res.json({msg:'File uploaded to ' + uploadPath});
-    });
+    }); */
 }
 
 export{
